8a32a61e15e591b1187ee7b23bb02c88722506d3
David <david_5043@hotmail.com>
2016-12-28 14:38:23 -0600

    basic interface created, messages from buttons added

--- app/src/main/java/geoquiz/david/com/geoquiz/MainActivity.java
+++ app/src/main/java/geoquiz/david/com/geoquiz/MainActivity.java
@@ -2,12 +2,39 @@ package geoquiz.david.com.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //Definicion de los botones
+    private Button mTrueButton;
+    private Button mFalseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //casting buttons
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        //true button setter
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //toast -> display message in screen
+                Toast.makeText(MainActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //false button setter
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, R.string.incorrect_toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
